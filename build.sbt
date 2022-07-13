da4d0272618efcdef0707af13fbf7b9933a71466
chaitnayabasava <chaitanya.basava@gmail.com>
2022-07-13 23:14:01 +0530

    improved producer code

--- build.sbt
+++ build.sbt
@@ -9,5 +9,13 @@ lazy val root = (project in file("."))
 
 libraryDependencies += "com.typesafe" % "config" % "1.4.2"
 libraryDependencies += "org.apache.kafka" %% "kafka" % "3.1.0"
-libraryDependencies += "com.lihaoyi" %% "upickle" % "2.0.0"
-libraryDependencies += "com.lihaoyi" %% "requests" % "0.7.1"
+
+libraryDependencies ++= Seq(
+  "com.lihaoyi" %% "upickle" % "2.0.0",
+  "com.lihaoyi" %% "requests" % "0.7.1"
+)
+
+libraryDependencies ++= Seq(
+  "org.slf4j" % "slf4j-api" % "1.7.36",
+  "org.slf4j" % "slf4j-simple" % "1.7.36"
+)
--- src/main/scala/com/tweet/stream/TestProducer.scala
+++ src/main/scala/com/tweet/stream/TestProducer.scala
@@ -1,25 +1,51 @@
 package com.tweet
 package stream
 
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
+import org.apache.kafka.common.serialization.StringSerializer
+import org.slf4j.{Logger, LoggerFactory}
 
 import java.util.Properties
 
-object TestProducer {
-  def main(args: Array[String]): Unit = {
-    writeToKafka(topic = args(0), bootstrap_server = KAFKA_BOOTSTRAP_SERVER)
-  }
+class TestProducer {
+  private val log: Logger = LoggerFactory.getLogger(classOf[TestProducer])
 
   def writeToKafka(topic: String, bootstrap_server: String): Unit = {
     val props = new Properties()
-    props.put("bootstrap.servers", bootstrap_server)
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap_server)
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
 
     val producer = new KafkaProducer[String, String](props)
     val record = new ProducerRecord[String, String](topic, "ex-key", "ex_value")
 
-    producer.send(record)
+    // asynchronous operation and won't wait till record sent to topic
+    producer.send(record, new Callback {
+      override def onCompletion(metadata: RecordMetadata, e: Exception): Unit = {
+        // executed whenever record is sent successfully or when an exception is thrown
+        if (e == null) {
+          log.info(
+            s"Topic: ${metadata.topic()}\n " +
+              s"Key: ${record.key()}" +
+              s"Partition: ${metadata.partition()}" +
+              s"Offset: ${metadata.offset()}\n " +
+              s"Timestamp: ${metadata.timestamp()}"
+          )
+        }
+      }
+    })
+
+    // will flush and close - flush is synchronous and needed to prevent
+    // application from closing before producer can send data to topic
     producer.close()
+  }
+}
+
+object TestProducer {
+  def main(args: Array[String]): Unit = {
+    val test = new TestProducer()
+    test.log.info("Started producer!")
+
+    test.writeToKafka(topic = args(0), bootstrap_server = KAFKA_BOOTSTRAP_SERVER)
   }
 }
